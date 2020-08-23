3576250d5385303559b9d1571c893a3dd7ebc0df
Egor <mrzillagold@gmail.com>
2020-08-24 04:39:03 +1000

    Use other quotes type

--- src/WrapperError.mjs
+++ src/WrapperError.mjs
@@ -2,18 +2,18 @@ export class WrapperError {
 
     constructor() {
         this.errors = [
-            `Unknown error.\n$params`, // 0
-            `Axios request error.\n$params`, // 1
-            `Invalid Minecraft nickname. (/^[A-Za-z0-9_]{2,16}$/)`, // 2
-            `Nickname $params does not exist.`, // 3
-            `There is no useful information in the data received, if the problem repeats please report this to the Github issue.`, // 4
-            `Skin hash $params does not exist.`, // 5
-            `Invalid argument type $params. Check available types in docs.`, // 6
-            `The required parameters were not passed to the method. Check the necessary parameters in docs.` // 7
+            "Unknown error.\n$params", // 0
+            "Axios request error.\n$params", // 1
+            "Invalid Minecraft nickname. (/^[A-Za-z0-9_]{2,16}$/)", // 2
+            "Nickname $params does not exist.", // 3
+            "There is no useful information in the data received, if the problem repeats please report this to the Github issue.", // 4
+            "Skin hash $params does not exist.", // 5
+            "Invalid argument type $params. Check available types in docs.", // 6
+            "The required parameters were not passed to the method. Check the necessary parameters in docs." // 7
         ];
     }
 
-    get(code, params) {
+    get(code, params = "") {
         const errors = this.errors;
 
         return {
@@ -21,7 +21,7 @@ export class WrapperError {
                 errors[code] ?
                     {
                         code,
-                        reason: errors[code].replace("$params", params || "")
+                        reason: errors[code].replace("$params", params)
                     }
                     :
                     {
