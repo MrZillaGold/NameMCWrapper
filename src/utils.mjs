bf069dfda4f144fcd2ac3ddb229ab01598a846e0
Пронин Егор <mrzillagold@gmail.com>
2020-12-21 04:25:00 +1000

    Improve uuid regexp

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,4 +1,4 @@
-const nameRegExp = /^(?:(?<name>[A-Za-z0-9_]{1,16})|(?<uuid>[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}))$/;
+const nameRegExp = /^(?:(?<name>[A-Za-z0-9_]{1,16})|(?<uuid>[0-9a-f]{8}-?[0-9a-f]{4}-?[0-5][0-9a-f]{3}-?[089ab][0-9a-f]{3}-?[0-9a-f]{12}))$/;
 const profileRegExp = /[^]+\/profile\/[^]+/;
 const skinRegExp = /[^]+\/skin\/([^]+)/;
 
