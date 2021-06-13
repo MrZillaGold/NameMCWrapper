58dc9a695c831940aa58eeec7fcf4f941319a70b
Пронин Егор <mrzillagold@gmail.com>
2021-06-14 05:25:33 +1000

    fix(interfaces): export contexts

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,14 @@
 export * from "./NameMC";
 export * from "./API";
 
-export { NameMC } from "../NameMC";
+export {
+    NameMC,
+    API,
+
+    RendersContext,
+    ServerContext,
+    SkinContext,
+    CapeContext,
+    PlayerContext
+} from "../NameMC";
 export * from "../API";
