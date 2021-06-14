666ff8e8db7e3ed7b2bba2b4e59f0b7dc41902da
Пронин Егор <mrzillagold@gmail.com>
2021-06-14 14:29:57 +1000

    fix: remove circular dependencies

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,14 +1,2 @@
 export * from "./NameMC";
 export * from "./API";
-
-export {
-    NameMC,
-    API,
-
-    RendersContext,
-    ServerContext,
-    SkinContext,
-    CapeContext,
-    PlayerContext
-} from "../NameMC";
-export * from "../API";
