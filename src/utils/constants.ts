927ba8a6675c2eea884f720055f5bf68a5b5a2a8
Pronin Egor <mrzillagold@gmail.com>
2022-03-07 08:29:48 +1000

    chore(constants): use uppers case

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const steveSkinHash = '12b92a9206470fe2';
+export const STEVE_SKIN_HASH = '12b92a9206470fe2';
 
 export const nameRegExp = /^(?:(?<name>[A-Za-z0-9_]{1,16})|(?<uuid>[0-9a-f]{8}-?[0-9a-f]{4}-?[0-5][0-9a-f]{3}-?[089ab][0-9a-f]{3}-?[0-9a-f]{12}))$/;
 export const profileRegExp = /(?:[^]+)?\/profile\/(?:([^]+)\.([\d]+)|([^]+))/;
